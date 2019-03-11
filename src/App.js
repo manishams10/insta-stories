6a756efc8c4c278bbfe476e3a13026c794ec5525
mavericovika <makarov1011@gmail.com>
2019-03-12 05:29:52 +0530

    Initial commit from Create React App
    Signed-off-by: mavericovika <makarov1011@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,28 +1,58 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Container from './component/Container'
+import PropTypes from 'prop-types'
+import axios from 'axios';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+export class App extends Component {
+  state = {
+      stories: []
   }
+
+    componentDidMount() {
+
+        axios.get('https://api.giphy.com/v1/gifs/trending?api_key=QiidCosXIDRAGim3GDaXAAVGWJFVsKBE&limit=5&rating=G')
+            .then(res=>{
+                let data = res.data.data;
+               let storiesarr = data.map(obj=>{
+                    return {
+                        url: obj.images.original.url,
+                        header: { heading: obj.user.display_name,
+                            subheading: 'Posted 1h ago',
+                            profileImage: obj.user.avatar_url }
+                    }
+                })
+
+                this.setState({stories: storiesarr});
+            })
+
+
+    }
+
+    render() {
+
+        return (
+            <div>
+                <Container
+                   stories={this.state.stories}
+                    defaultInterval={this.props.defaultInterval}
+                    loader={this.props.loader}
+                    header={this.props.header}
+                />
+            </div>
+        )
+    }
 }
+
+
+
+
+App.propTypes = {
+    defaultInterval: PropTypes.number,
+    loader: PropTypes.element,
+    header: PropTypes.element
+}
+
+
+
 
 export default App;
